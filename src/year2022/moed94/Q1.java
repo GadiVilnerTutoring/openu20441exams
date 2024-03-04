2255587811bcc34bea0433f7065b227314e1965d
Gadi Vilner <gadi.tutoring@gmail.com>
2024-03-04 16:45:55 +0200

    added method for finding a none existing char in pattern 2022/94

--- src/year2022/moed94/Q1.java
+++ src/year2022/moed94/Q1.java
@@ -4,25 +4,33 @@ public class Q1
 {
 	public static int lengthPath(char[][] mat, String pattern)
 	{
-		return lengthPath(mat, pattern, 0, 0);
+		return lengthPath(mat, pattern, 0, 0, findFreeChar(pattern, 'a'));
 	}
 
-	private static int lengthPath(char[][] mat, String pattern, int i, int j)
+	private static int lengthPath(char[][] mat, String pattern, int i, int j, char ch)
 	{
-		if (i < 0 || i == mat.length || j < 0 || j == mat[i].length || pattern.indexOf(mat[i][j]) < 0 || mat[i][j] == ' ')
+		if (i < 0 || i == mat.length || j < 0 || j == mat[i].length || pattern.indexOf(mat[i][j]) < 0)
 		{
 			return 0;
 		}
 
 		char temp = mat[i][j];
-		mat[i][j] = ' ';
-		int r0 = lengthPath(mat, pattern, i + 1, j);
-		int r1 = lengthPath(mat, pattern, i - 1, j);
-		int r2 = lengthPath(mat, pattern, i, j + 1);
-		int r3 = lengthPath(mat, pattern, i, j - 1);
+		mat[i][j] = ch;
+		int r0 = lengthPath(mat, pattern, i + 1, j, ch);
+		int r1 = lengthPath(mat, pattern, i - 1, j, ch);
+		int r2 = lengthPath(mat, pattern, i, j + 1, ch);
+		int r3 = lengthPath(mat, pattern, i, j - 1, ch);
 		mat[i][j] = temp;
 		return Math.max(Math.max(r0, r1), Math.max(r2, r3)) + 1;
 	}
+
+	private static char findFreeChar(String pattern, char ch) {
+		if (pattern.indexOf(ch) < 0){
+			return ch;
+		}
+		return findFreeChar(pattern, (char)(ch+1));
+	}
+
 
 	public static int maxPath(char[][] mat, String pattern)
 	{
@@ -40,7 +48,7 @@ public class Q1
 			return maxPath(mat, pattern, i + 1, 0);
 		}
 
-		int length = lengthPath(mat, pattern, i, j);
+		int length = lengthPath(mat, pattern, i, j, findFreeChar(pattern, 'a'));
 		return Math.max(length, maxPath(mat, pattern, i, j + 1));
 	}
 }
